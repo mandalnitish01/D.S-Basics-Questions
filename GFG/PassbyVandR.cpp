1787334cf81b7976d1d207da8a5c789ef5224124
nitish <mandalnitish570@gmail.com>
2025-01-23 09:46:59 +0530

    23 jan

--- GFG/PassbyVandR.cpp
+++ GFG/PassbyVandR.cpp
@@ -5,7 +5,6 @@ int PassedBy(int a,int &b){
     int ans2 = b +2;
   return  ans2;  // Modify the reference parameter b to reflect the change
     return ans1;  // Returning ans1
-
 }
 int main(){
     int a;
