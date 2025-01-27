41f720cd9cd3b95423dee9de7381789397be0dbb
nitish <mandalnitish570@gmail.com>
2025-01-27 12:04:43 +0530

    Add few solutions

--- Programming/Factorial.cpp
+++ Programming/Factorial.cpp
@@ -3,14 +3,17 @@ using namespace std;
 int main()
 {
     int userinput;
-     int fact = 1;
+     
     cout << "Enter a number: ";
     cin>> userinput;
     cout<<endl;
+    int fact = 1;
     for(int i=1;i<=userinput;i++){
         fact = fact * i;
     }
-    cout<<"factorial =";
+    cout<<"factorial = ";
     cout<<fact;
+    cout<<endl;
     return 0;
 }
+// Factorial of a number is the product of all the integers from 1 to that number. For example, the factorial of 6 is 1*2*3*4*5*6 = 720.
