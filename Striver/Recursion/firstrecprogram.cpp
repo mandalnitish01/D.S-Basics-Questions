72dbd3f950c2bea34fd076ff4f97960c377554b9
nitish <mandalnitish570@gmail.com>
2025-01-27 22:15:17 +0530

    Add 2 number using recursion function

--- Striver/Recursion/firstrecprogram.cpp
+++ Striver/Recursion/firstrecprogram.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
-
+void addfun2(int n1, int n2){
+    int sum = 0;
+    sum = n1 + n2;
+    cout<<"sum = "<<sum<<endl;
+}
 void addfun(int n1, int n2){
     int sum = 0;
     sum = n1 + n2;
     cout<<"sum = "<<sum<<endl;
-
-
+    addfun2(2, 4);
 }
 int main(){
     int n1,n2;
@@ -14,3 +17,9 @@ int main(){
     addfun(n1,n2);
     return 0;
 }
+
+// output 
+// 10
+// 12
+// sum = 22
+// sum = 6
